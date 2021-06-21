48eb196110dcfc73676025bb4b6f7f4dc0ef4cdc
nopalhiya <mamangque@gmail.com>
2021-06-21 14:23:21 +0700

    penambahan migaration + model sosmed, visimisi

---
+++ database/migrations/2021_06_21_070601_create_social_media_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_medias', function (Blueprint $table) {
+            $table->id();
+            $table->string('website', 100)->nullable();
+            $table->string('instagram', 30)->nullable();
+            $table->string('youtube')->nullable();
+            $table->foreignId('ormawas_id');
+            $table->foreign('ormawas_id')->references('id')->on('ormawas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_medias');
+    }
+}
