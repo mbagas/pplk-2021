df29738f7df41778e7da046c0013271bcd868fc2
nopalhiya <mamangque@gmail.com>
2021-06-28 14:58:30 +0700

    js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,20 +25,6 @@
     <!--*******************
         Preloader start
     ********************-->
-    <div id="preloader">
-        <div class="waviy">
-		   <span style="--i:1">L</span>
-		   <span style="--i:2">o</span>
-		   <span style="--i:3">a</span>
-		   <span style="--i:4">d</span>
-		   <span style="--i:5">i</span>
-		   <span style="--i:6">n</span>
-		   <span style="--i:7">g</span>
-		   <span style="--i:8">.</span>
-		   <span style="--i:9">.</span>
-		   <span style="--i:10">.</span>
-		</div>
-    </div>
     <!--*******************
         Preloader end
     ********************-->
