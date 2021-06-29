7b21804985a00fee3a1bcc61608625de1afc7983
mbagas <karab4946@gmail.com>
2021-06-29 14:48:41 +0700

    add create user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'nama' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -65,9 +65,14 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'nama' => $data['nama'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'nim' => $data['nim'],
+            'kelompok' => $data['kelompok'],
+            'roles_id' => $data['roles_id'],
+            'prodis_id' => $data['prodis_id'],
+            'instagram' => $data['instagram'],
         ]);
     }
 }
