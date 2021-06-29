6496df37659e80e9944486ea17f8d6e53d51e673
lngalmsyr <lanang.al2610@gmail.com>
2021-06-30 03:17:41 +0700

    bagian ukm dan pplk

--- resources/views/dashboard/content/Ukm/updateUkm.blade.php
+++ resources/views/dashboard/content/Ukm/updateUkm.blade.php
@@ -1,97 +1,174 @@
 @extends('layouts.master')
 @section('title', 'Update UKM')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Update UKM</h4>
+
+<div class="col-xl-12 col-lg-12 form-wrapper">
+    <h1 class="content-title">Perbarui UKM/Komunitas</h1>
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">UKM/Komunitas</h4>
+        </div>
+        <div class="card-body">
+        <div class="basic-form">
+            <form method="POST" action="{{ route('dashboard.ukm.update', $result->ormawas->id) }}" enctype='multipart/form-data'>
+                @method('PUT')
+                @csrf
+
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Nama Singkat</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Nama Singkat" name="namaSingkat"
+                            id="namaSingkat" value="{{ $result->ormawas->namaSingkat }}" required disabled>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('dashboard.ukm.update', $result->ormawas->id) }}">
-                        @method('PUT')
-                        @csrf
-                        <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Nama Lengkap</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="nama" class="form-control form-control-lg" value="{{ $result->ormawas->namaLengkap }}" required>
-                            </div>
-                            </div>
-                            <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Nama Singkat</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="namaSingkat" class="form-control form-control-lg" value="{{ $result->ormawas->namaSingkat }}" required>
-                            </div>
-                            </div>
 
-                            <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Pembina</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="pembina" class="form-control form-control-lg" value="{{ $result->pembina }}" required>
-                            </div>
-                            </div>
-                            <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Ketua Umum</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="ketuaUmum" class="form-control form-control-lg" value="{{ $result->ketuaUmum }}" required>
-                            </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Tahun Berdiri</label>
-                            <div class="col-sm-10">
-                                <input type="number" name="tahunBerdiri" class="form-control form-control-lg" value="{{ $result->tahunBerdiri }}" required>
-                            </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                                <label class="col-sm-2 col-form-label col-form-label-lg">Website</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="website" class="form-control form-control-lg">
-                                </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                                <label class="col-sm-2 col-form-label col-form-label-lg">Youtube</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="youtube" class="form-control form-control-lg">
-                                </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                                <label class="col-sm-2 col-form-label col-form-label-lg">Instagram</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="instagram" class="form-control form-control-lg">
-                                </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                                <label class="col-sm-2 col-form-label col-form-label-lg">Visi</label>
-                                <div class="col-sm-10">
-                                    <input type="text" name="visi" class="form-control form-control-lg" required>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                                <label class="col-sm-2 col-form-label col-form-label-lg">Misi</label>
-                                <div class="col-sm-10">
-                                    <textarea type="text" name="Misi" class="form-control form-control-lg" required></textarea>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label col-form-label-lg">Deskripsi</label>
-                            <div class="col-sm-10">
-                                <textarea type="text" name="artikel" class="form-control form-control-lg" required>{{ $artikel->body }}</textarea>
-                            </div>
-                            </div>
-                        <button type="submit" class="btn btn-lg btn-primary">Update</button>
-
-                    </form>
-
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Nama Lengkap</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Nama Lengkap" name="namaLengkap"
+                            id="namaLengkap" value="{{ $result->ormawas->namaLengkap }}" required disabled>
+                    </div>
                 </div>
-            </div>
+
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Pembina</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Pembina" name="pembina" id="pembina"
+                            value="{{ $result->pembina }}" required disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Ketua Umum</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Ketua Umum" name="ketuaUmum" id="ketuaUmum"
+                            value="{{ $result->ketuaUmum }}" required disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Tahun Berdiri</label>
+                    <div class="col-sm-9">
+                        <input type="date" class="form-control" name="tahunBerdiri" id="tahunBerdiri"
+                            value="{{ $result->tahunBerdiri }}" required disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Visi</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Visi" name="visi" id="visi" value="{{ $result->ormawas->visi }}" required
+                            disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Misi</label>
+                    <div class="col-sm-9">
+                        <textarea class="form-control custom-txt-area misi" placeholder="Misi" name="misi" id="misi"
+                            value="{{ $result->ormawas->misi }}" required disabled></textarea>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Logo</label>
+                    <div class="col-sm-9">
+                        <input type="file" accept="image/*" class="form-control" name="logo" id="logo" value="{{ $result->logo }}"
+                            required disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Filosofi Logo</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Filosofi Logo" name="filosofiLogo"
+                            id="filosofiLogo" value="{{ $result->filosofiLogo }}" disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Barcode</label>
+                    <div class="col-sm-9">
+                        <input type="file" accept="image/*" class="form-control" name="barcode" id="barcode"
+                            value="{{ $result->barcode }}" required disabled>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Prestasi</label>
+                    <div class="col-sm-9 ">
+                        <textarea class="form-control custom-txt-area prestasi" placeholder="Prestasi" name="prestasi"
+                            id="prestasi" value="{{ $result->prestasi }}" required disabled></textarea>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Deskripsi</label>
+                    <div class="col-sm-9 ">
+                        <textarea class="form-control custom-txt-area" placeholder="Deskripsi" name="deskripsi"
+                            id="deskripsi" value="{{ $result->deskripsi }}" required disabled></textarea>
+                    </div>
+                </div>
+                <div class="accordion accordion-danger-solid" id="accordion-two">
+                    <div class="accordion-item">
+                        <div class="accordion-header rounded-lg collapsed" id="accord-2One" data-bs-toggle="collapse"
+                            data-bs-target="#collapse2One" aria-controls="collapse2One" aria-expanded="false"
+                            role="button">
+                            <span class="accordion-header-text">Sosial Media</span>
+                            <span class="accordion-header-indicator"></span>
+                        </div>
+                        <div id="collapse2One" class="accordion__body collapse" aria-labelledby="accord-2One"
+                            data-bs-parent="#accordion-two">
+                            <div class="accordion-body-text">
+                                <div class="mb-3 row">
+                                    <label class="col-sm-3 col-form-label">Website</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" placeholder="Website" name="website"
+                                            id="website" value="{{ $result->ormawas->website }}" required disabled>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label class="col-sm-3 col-form-label">Instagram</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" placeholder="Instagram" name="instagram"
+                                            id="instagram" value="{{ $result->ormawas->instagram }}"  required disabled>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label class="col-sm-3 col-form-label">Youtube</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" placeholder="Youtube" name="youtube"
+                                            id="youtube" value="{{ $result->ormawas->youtube }}" required disabled>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3 row">
+                        <div class="col-sm-9">
+                          <button type="submit" class="btn btn-primary " >Simpan</button>
+                          <button type="button" class="btn btn-outline-primary " onclick="editData()">Edit</button>
+                        </div>
+                    </div>
+            </form>
+        </div>
         </div>
     </div>
+</div>
 
+@endsection
+
+@section('script')
+<script>
+    function editData() {   
+            document.getElementById('namaLengkap').disabled = false;
+			document.getElementById('namaSingkat').disabled = false;
+			document.getElementById('pembina').disabled = false;
+			document.getElementById('ketuaUmum').disabled = false;
+			document.getElementById('tahunBerdiri').disabled = false;
+			document.getElementById('visi').disabled = false;
+			document.getElementById('misi').disabled = false;
+			document.getElementById('logo').disabled = false;
+			document.getElementById('filosofiLogo').disabled = false;
+			document.getElementById('barcode').disabled = false;
+			document.getElementById('prestasi').disabled = false;
+			document.getElementById('deskripsi').disabled = false;
+			document.getElementById('website').disabled = false;
+			document.getElementById('instagram').disabled = false;
+			document.getElementById('youtube').disabled = false;
+    }
+</script>
 @endsection
