35d34deb3e87683bd2f99f4c2bb496cadfcce2f2
KRSBX <firdaus.sati51@gmail.com>
2021-06-30 13:39:48 +0700

    Adding CRUD Tugas
    All Views
    CRUD

--- app/Http/Controllers/tugasController.php
+++ app/Http/Controllers/tugasController.php
@@ -3,82 +3,172 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Tugas;
+use Exception;
 
 class tugasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $result = Tugas::all();
+
+    return view('tugas.content.tugas', compact('result'));
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('tugas.content.tambahTugas');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+
+  /*
+    Request:
+    // Tugas
+        judul
+        deskripsi
+        format
+        file
+        start_time
+        end_time
+  */
+  public function store(Request $request)
+  {
+    try {
+      $tugas = new Tugas();
+      $tugas->judul = $request->judul;
+      $tugas->deskripsi = $request->deskripsi;
+      $tugas->format = $request->format;
+
+      $file = $this->SaveFiles($request);
+
+      $tugas->file = $file;
+
+      $tugas->start_time = $request->start_time;
+      $tugas->end_time = $request->end_time;
+
+      $tugas->save();
+    } catch (Exception $err) {
+      return redirect('dashboard/tugas')->with('error', 'Gagal Menambahkan Data!');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    return redirect('dashboard/tugas')->with('sukses', 'Berhasil Menambahkan Data!');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+
+    try {
+      $tugas = Tugas::where('id', $id)->firstOrFail();
+
+      return view('tugas.content.updateTugas', compact('tugas'));
+    } catch (Exception $err) {
+      return redirect('dashboard/tugas')->with('error', 'Gagal Menyunting Data!');
+    }
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+
+  /*
+    Request:
+    // Tugas
+        judul
+        deskripsi
+        format
+        file
+        start_time
+        end_time
+  */
+  public function update(Request $request, $id)
+  {
+    try {
+      $tugas = Tugas::where('id', $id)->firstOrFail();
+      $tugas->judul = $request->judul;
+      $tugas->deskripsi = $request->deskripsi;
+      $tugas->format = $request->format;
+
+      if ($request->file !== null) {
+        $file = $this->SaveFiles($request);
+
+        $tugas->file = $file;
+      }
+
+      error_log('Start : ' . $request->start_time);
+      error_log('End : ' . $request->end_time);
+
+      if ($request->start_time !== null) {
+        $tugas->start_time = $request->start_time;
+      }
+
+      if ($request->start_time !== null) {
+        $tugas->end_time = $request->end_time;
+      }
+
+      $tugas->save();
+    } catch (Exception $err) {
+      return redirect('dashboard/tugas')->with('error', 'Gagal Memperbarui Data!');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return redirect('dashboard/tugas')->with('sukses', 'Berhasil Memperbarui Data!');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $tugas = Tugas::where('id', $id)->firstOrFail();
+    Storage::delete($tugas->file);
+    $tugas->delete();
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  public function SaveFiles(Request $request)
+  {
+    $namaFiles = $request->judul . '.' . $request->file->extension();
+    $request->file->storeAs(('Tugas'), $namaFiles);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    return $namaFiles;
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\ukmController;
 use App\Http\Controllers\jurusanController;
 use App\Http\Controllers\senatController;
 use App\Http\Controllers\kamusGaulController;
-
+use App\Http\Controllers\tugasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,43 +33,42 @@ Route::post('/', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['Admin'])->name('dashboard.')->prefix('dashboard')->group(function () {
-    // Ini Route yang hanya bisa diakses Role SuperAdmin
+  // Ini Route yang hanya bisa diakses Role SuperAdmin
 
-    // SUDAH DI OPTIMIZE (routing)
-    Route::get('/', [dashboardController::class, 'index'])->name('index');
+  // SUDAH DI OPTIMIZE (routing)
+  Route::get('/', [dashboardController::class, 'index'])->name('index');
 
-    Route::resource('prodi', prodiController::class)->except(['show']);
-    Route::resource('ukm', ukmController::class)->except(['show']);
-    Route::resource('himpunan', himpunanController::class)->except(['show']);
-    Route::resource('funfact', FunFactController::class)->except(['show']);
-    Route::resource('jurusan', jurusanController::class)->except(['show']);
-    Route::resource('senat', senatController::class)->except(['show']);
-    Route::resource('pplk', pplkController::class)->except('show');
-    Route::resource('upt', uptController::class)->except('show');
-    Route::resource('kamusGaul', kamusGaulController::class)->except('show');
+  Route::resource('prodi', prodiController::class)->except(['show']);
+  Route::resource('ukm', ukmController::class)->except(['show']);
+  Route::resource('himpunan', himpunanController::class)->except(['show']);
+  Route::resource('funfact', FunFactController::class)->except(['show']);
+  Route::resource('jurusan', jurusanController::class)->except(['show']);
+  Route::resource('senat', senatController::class)->except(['show']);
+  Route::resource('pplk', pplkController::class)->except('show');
+  Route::resource('upt', uptController::class)->except('show');
+  Route::resource('kamusGaul', kamusGaulController::class)->except('show');
 
-    // Route::get('/data-mahasiswa', [dataMahasiswa::class, 'index'])->name('data-mahasiswa');
-    // Route::post('/create', [dataMahasiswa::class, 'addMahasiswa'])->name('addMahasiswa');
+  // Route::get('/data-mahasiswa', [dataMahasiswa::class, 'index'])->name('data-mahasiswa');
+  // Route::post('/create', [dataMahasiswa::class, 'addMahasiswa'])->name('addMahasiswa');
 
-//    Route::get('/data-mahasiswa', [dataMahasiswa::class, 'index'])->name('data-mahasiswa');
-//    Route::post('/create', [dataMahasiswa::class, 'addMahasiswa'])->name('addMahasiswa');
-
+  //    Route::get('/data-mahasiswa', [dataMahasiswa::class, 'index'])->name('data-mahasiswa');
+  //    Route::post('/create', [dataMahasiswa::class, 'addMahasiswa'])->name('addMahasiswa');
+  Route::resource('tugas', tugasController::class)->except('show');
 });
 
 
 Route::middleware(['Tugas'])->group(function () {
-    // Ini Route yang hanya bisa diakses Role Tugas
+  // Ini Route yang hanya bisa diakses Role Tugas
 });
 
 Route::middleware(['DaplokMentor'])->group(function () {
-    // Ini Route yang hanya bisa diakses Role DaplokMentor
+  // Ini Route yang hanya bisa diakses Role DaplokMentor
 });
 
 Route::middleware(['Pendanaan'])->group(function () {
-    // Ini Route yang hanya bisa diakses Role Pendanaan
+  // Ini Route yang hanya bisa diakses Role Pendanaan
 });
 
 Route::middleware(['Maba'])->group(function () {
-    // Ini Route yang hanya bisa diakses Role Maba
+  // Ini Route yang hanya bisa diakses Role Maba
 });
-
