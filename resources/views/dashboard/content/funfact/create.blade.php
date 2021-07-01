298836ef4ec617db7e02114ae3ba3743cdce97ad
hfdzz <yhafidz@gmail.com>
2021-07-01 17:26:01 +0700

    create.blade.php html tag
    Tambahan tag html yang sesuai.

--- resources/views/dashboard/content/funfact/create.blade.php
+++ resources/views/dashboard/content/funfact/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 @section('title', 'Tambah Funfact')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
+        <div class="col-xl-12 col-lg-12 form-wrapper">
+			<h1 class="content-title">Masukkan Data Fun Fact</h1>
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Tambah FunFact</h4>
+                    <h4 class="card-title">Fun Fact</h4>
                 </div>
                 <div class="card-body">
 
@@ -16,21 +16,24 @@
                             </div>
                         @endforeach
                     @endif
-                    <form method="POST" action="{{ route('dashboard.funfact.store') }}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="mb-3 row form-group">
-                            <label for="body">Body</label>
-                            <textarea name="body" class="form-control" id="body" rows="5" required></textarea>
-                            @error('body')
-                            <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <button type="submit" class="btn btn-lg btn-primary">Tambah</button>
-                    </form>
+                    <div class="basic-form">
+                        <form method="POST" action="{{ route('dashboard.funfact.store') }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3 row">
+                                <label class="col-sm-3 col-form-label">Fun Fact</label>
+                                <div class="col-sm-9">
+                                    <textarea class="form-control custom-txt-area" placeholder="Fun Fact" name="body" id="body" required></textarea>
+                                </div>
+                                @error('body')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-lg btn-primary">Tambah</button>
+                        </form>
+                    </div>
 
                 </div>
             </div>
         </div>
-    </div>
 
 @endsection
