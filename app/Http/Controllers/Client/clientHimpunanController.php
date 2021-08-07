39d221a57a1097d22320f2e8530d1dfe1e4c3951
rahmanfajri <rahmanfajri.rf@gmail.com>
2021-08-08 00:39:17 +0700

    index himpunan client side

--- app/Http/Controllers/Client/clientHimpunanController.php
+++ app/Http/Controllers/Client/clientHimpunanController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Himpunan;
+use App\Models\Ormawa;
 
 class clientHimpunanController extends Controller
 {
     //
     public function index(){
-        return view('client.himpunan');
+        $himpunans = Cache::remember('himpunans', 3600, function (){
+            return Himpunan::with('ormawas')->get();
+        });
+        return view('client.himpunan', compact('himpunans'));
     }
 }
