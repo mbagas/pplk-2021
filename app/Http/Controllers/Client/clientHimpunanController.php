a80411295cd24179972d18208620cc2dce904e5b
rahmanfajri <rahmanfajri.rf@gmail.com>
2021-08-08 00:53:33 +0700

    update client Himpunan

--- app/Http/Controllers/Client/clientHimpunanController.php
+++ app/Http/Controllers/Client/clientHimpunanController.php
@@ -6,14 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Himpunan;
 use App\Models\Ormawa;
+use App\Models\VisiMisi;
+use App\Models\Artikel;
 
 class clientHimpunanController extends Controller
 {
     //
     public function index(){
-        $himpunans = Cache::remember('himpunans', 3600, function (){
-            return Himpunan::with('ormawas')->get();
+        $himpunans = cache()->remember('himpunans', 7200, function () {
+            return Himpunan::with("ormawas")->get();
         });
         return view('client.himpunan', compact('himpunans'));
+    }
+
+    public function show(Ormawa $himpunan){
+            $dataHimpunan = Himpunan::with('ormawas')->where('ormawas_id', $himpunan->id)->firstOrFail();
+        	$dataVisiMisi = VisiMisi::with('ormawas')->where('ormawas_id', $himpunan->id)->firstOrFail();
+        	$dataArtikel = Artikel::with('ormawas')->where('ormawas_id', $himpunan->id)->firstOrFail();
+        return view('client.himpunanDetail2',compact('dataHimpunan','himpunan','dataVisiMisi','dataArtikel'));
     }
 }
