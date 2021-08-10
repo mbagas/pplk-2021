985b867bc1d4f6383650561c6ae8fc9351a177fc
dhifafaz <dhifaf.az@gmail.com>
2021-08-10 16:45:54 +0700

    upt fix

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -408,7 +408,7 @@
                             Kamus Gaul
                         </a>
 
-                        <a href="../html/uptItera.html" class="small-slide-menu-item justify-content-center">
+                        <a href="{{ route('dataUpt') }}" class="small-slide-menu-item justify-content-center">
                             UPT ITERA
                         </a>
 
