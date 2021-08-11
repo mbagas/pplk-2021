6aea224bdc77f60a9c12a2fc23999d9fe586887b
dhifafaz <dhifaf.az@gmail.com>
2021-08-12 02:25:17 +0700

    komen

--- resources/views/client/games.blade.php
+++ resources/views/client/games.blade.php
@@ -94,7 +94,7 @@
                                 <div class="result-points-wrapper">
                                     <canvas id="result-canvas" class="result-canvas"></canvas>
                                     <div class="result-canvas-text">
-                                        <!-- Progress Circle auto ke id="canvas-percent" -->
+                                        <!-- Progress Circle auto ke id="canvas-percent" tinggal kasih data di spannya-->
                                         <span id="canvas-percent" class="canvas-percent title">18</span>
                                         <span class="canvas-text bodytext">%</span>
                                     </div>
@@ -118,7 +118,7 @@
                                 <div class="result-points-wrapper">
                                     <canvas id="result-canvas-2" class="result-canvas"></canvas>
                                     <div class="result-canvas-text">
-                                        <!-- Progress Circle auto ke id="canvas-percent" -->
+                                        <!-- Progress Circle auto ke id="canvas-percent-2" tinggal kasih data di spannya-->
                                         <span id="canvas-percent-2" class="canvas-percent title">90</span>
                                         <span class="canvas-text bodytext">%</span>
                                     </div>
