662ef5e2b3151d79991c7d26e5934d1ef9e1c210
Akmal Fauzan Suranta <akmalfauzan.af@gmail.com>
2021-08-13 16:02:58 +0700

    kabinet fixing

--- app/Http/Controllers/Client/clientKabinetController.php
+++ app/Http/Controllers/Client/clientKabinetController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kabinet;
 use Illuminate\Http\Request;
 
 class clientKabinetController extends Controller
 {
     //
     public function index(){
-        return view('client.kabinet');
+        $dataKabinet = Kabinet::first();
+        return view('client.kabinet', compact('dataKabinet'));
     }
 }
